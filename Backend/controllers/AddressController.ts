8951fdf0184a146786f946f2f9d0ac83ac48c937
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2024-12-24 00:10:51 -0300

    adding addresses

--- Backend/controllers/AddressController.ts
+++ Backend/controllers/AddressController.ts
@@ -1,6 +1,35 @@
 import { Request, Response } from "express";
 import { addressModel } from "../models/Address";
+import getTokenInfo from "../helpers/getTokenInfo";
 
 export class AddressController {
+  static async register(req: Request, res: Response) {
+    const {
+      bairro,
+      cep,
+      estado,
+      localidade,
+      logradouro,
+      regiao,
+      uf,
+      complemento,
+    } = req.body;
 
+    const tokenInfo = await getTokenInfo(req);
+    const userId = tokenInfo.id;
+
+    const newAddress = await addressModel.create({
+        bairro,
+        cep,
+        estado,
+        localidade,
+        logradouro,
+        regiao,
+        uf,
+        complemento,
+        userId
+    });
+
+    res.status(201).json(newAddress);
+  }
 }
