a81d972586e6cba36d0bdf639679d0e2118b2e19
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2024-12-24 02:44:13 -0300

    installing and configuring multer

---
+++ Backend/helpers/imageUpload.ts
@@ -0,0 +1,16 @@
+import multer from "multer";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const imageStore = multer.diskStorage({
+  destination: "public/images/products",
+  filename: function (req, file, cb) {
+    const photoId = uuidv4();
+    cb(null, photoId);
+  },
+});
+
+const imageUpload = multer({
+    storage: imageStore,
+    
+})
