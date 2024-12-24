5b9972d21803b5b0f1897392c4fe4ea70294c212
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2024-12-24 02:13:18 -0300

    creating basics products files

---
+++ Backend/controllers/ProductController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { productModel } from "../models/Product";
+
+
+export default class ProductController {
+
+}
---
+++ Backend/models/Product.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    amount: { type: Number, required: true },
+    photos: { type: Array, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const productModel = model("Products", productSchema);
