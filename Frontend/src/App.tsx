5cbb970b4af9673a99d16c4524f069a4a3755b6e
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2025-02-28 02:59:54 -0300

    intall shadcn/ui

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1>E-comerce</h1>
+      <Button  className="w-24 h-9">Botão</Button>
     </>
   );
 }
